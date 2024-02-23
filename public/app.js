35d8a440f7c170bac52438c002a238b3bd9234ca
MuhammadIlhamDev <nuaing2005@gmail.com>
2024-02-23 01:43:28 -0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const root = document.getElementById("root");
+const element = /*#__PURE__*/React.createElement("div", {
+  style: {
+    backgroundColor: "red",
+    width: 100,
+    height: 100
+  }
+});
+ReactDOM.render(element, root);
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const root = document.getElementById("root");
+
+const element = (
+  <div
+    style={{
+      backgroundColor: "red",
+      width: 100,
+      height: 100,
+    }}
+  ></div>
+);
+
+ReactDOM.render(element, root);
